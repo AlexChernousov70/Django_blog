d2caa06453622510c27b0992dab20a66eb15a147
Alex <alex70.07@mail.ru>
2025-06-22 18:41:25 +0300

    hw_45 Правка базового шаблона, создание главной страницы и пути к нему

--- exception_blog/urls.py
+++ exception_blog/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from blog import urls as blog_urls
 from users import urls as users_urls
+from users.views import LandingPageView
 # Импорт икнлюда
 from django.urls import include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", LandingPageView.as_view(), name="landing"),
     path('blog/', include(blog_urls, namespace='blog') ),
     path('users/', include(users_urls, namespace='users')),
 ]
