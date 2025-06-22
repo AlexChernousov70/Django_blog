2fa49bc75d4e7d33ac0d618b0fa7ec5e27119892
Alex <alex70.07@mail.ru>
2025-06-22 15:32:10 +0300

    hw_45 меняем настройки для отображения шаблонов

--- exception_blog/settings.py
+++ exception_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'exception_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Настройка маршрут для авторизации
+LOGIN_URL = "users:login"  # Имя маршрута для страницы входа
+LOGIN_REDIRECT_URL = (
+    "landing"  # Куда перенаправлять после успешного входа (если не указано в LoginView)
+)
+LOGOUT_REDIRECT_URL = "landing"  # Куда перенаправлять после выхода (используется LogoutView, если next_page не задан)
