18655dbc5dec7d30d3304761335099613707ee0a
Alex <alex70.07@mail.ru>
2025-06-22 10:29:50 +0300

    hw_45 Представление-заглушки для блога

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class BlogListView(TemplateView):
+    template_name = 'blog/index.html'
+    
+
+class BlogDetailView(TemplateView):
+    pass
+
